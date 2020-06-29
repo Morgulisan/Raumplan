c3c7dc931d4f66aa262322973861f6ae0cba114e
Malte <malte.ktz@hotmail.de>
2020-06-29 11:12:28 +0200

    Bildschirmprobleme, Commit um am anderen PC Weiter zu arbeiten

--- src/app/termin-anlegen/termin-anlegen.component.ts
+++ src/app/termin-anlegen/termin-anlegen.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-termin-anlegen',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./termin-anlegen.component.scss']
 })
 export class TerminAnlegenComponent implements OnInit {
+  @Input() appdata: any;
+
+  @Input('ngModel')
+  wessen: string;
+  wo: string;
+  wann: string;
+  dauer = 4;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initTimePicker();
   }
 
+  terminAnlegen(f: NgForm){
+    console.log('neuer Termin: ' +  f.value.wessen + ' ' + f.value.wo + ' ' + f.value.wann + ' ' + f.value.dauer);
+  }
+
+  initTimePicker(){
+    // Plain JS using Materialize.css
+    const timePickers = document.querySelectorAll('.timepicker');
+    // @ts-ignore
+    M.Timepicker.init(
+      timePickers,
+      {twelveHour: false, i18n: {cancel: 'Abbrechen', done: 'Fertig', clear: 'reset'}, container: 'container'}
+    );
+  }
 }
